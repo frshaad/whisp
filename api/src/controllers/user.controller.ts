01658568b28053aad71e381e2cd425d608d73396
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2024-09-04 14:41:53 +0330

    api: follow/unfollow

--- api/src/controllers/user.controller.ts
+++ api/src/controllers/user.controller.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express';
 import User from '../models/user.model';
+import { Types } from 'mongoose';
 
 export const getUserProfile = async (req: Request, res: Response) => {
-  const { username } = req.params;
   try {
+    const { username } = req.params;
     const user = await User.findOne({ username }).select('-password');
 
     if (!user) {
@@ -13,6 +14,56 @@ export const getUserProfile = async (req: Request, res: Response) => {
     }
 
     res.status(200).json({ status: 'success', user });
+  } catch (error) {
+    console.log('Error in get user profile controller', error);
+    res
+      .status(500)
+      .json({ status: 'failed', message: 'Internal server error' });
+  }
+};
+
+export const followUnfollowUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const currentUserId = (req.user?._id as string).toString();
+
+    if (currentUserId === id) {
+      return res.status(400).json({
+        status: 'failed',
+        message: 'You can not follow/unfollow yourself',
+      });
+    }
+
+    const currentUser = await User.findById(currentUserId);
+    const targetUser = await User.findById(id);
+
+    if (!targetUser || !currentUser) {
+      return res.status(404).json({
+        status: 'failed',
+        message: 'User not found',
+      });
+    }
+
+    const isInFollowing = currentUser?.following.includes(
+      id as unknown as Types.ObjectId,
+    );
+
+    if (isInFollowing) {
+      await User.findByIdAndUpdate(id, { $pull: { followers: currentUserId } });
+      await User.findByIdAndUpdate(currentUserId, { $pull: { following: id } });
+      res.status(200).json({
+        status: 'success',
+        message: 'User is successfully unfollowed',
+      });
+    } else {
+      await User.findByIdAndUpdate(id, { $push: { followers: currentUserId } });
+      await User.findByIdAndUpdate(currentUserId, { $push: { following: id } });
+      // Send notification
+      res.status(200).json({
+        status: 'success',
+        message: 'User is successfully followed',
+      });
+    }
   } catch (error) {
     console.log('Error in get user profile controller', error);
     res
--- api/src/routes/user.route.ts
+++ api/src/routes/user.route.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import authMiddleware from '../middlewares/auth.middleware';
-import { getUserProfile } from '../controllers/user.controller';
+import {
+  followUnfollowUser,
+  getUserProfile,
+} from '../controllers/user.controller';
 
 const router = Router();
 
 router.route('/profile/:username').get(authMiddleware, getUserProfile);
 router.route('/suggested').get(authMiddleware);
-router.route('/follow/:id').post(authMiddleware);
+router.route('/follow/:id').post(authMiddleware, followUnfollowUser);
 router.route('/update').post(authMiddleware);
 
 export default router;
