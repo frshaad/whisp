b9cc0e5ed28ae3977ddb76f0dad64548ebc013b0
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2024-09-07 17:06:59 +0330

    api: post controllers

--- api/src/controllers/post.contoller.ts
+++ api/src/controllers/post.contoller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from 'express';
 import { v2 as cloudinary } from 'cloudinary';
 import User from '../models/user.model';
 import Post from '../models/post.model';
+import mongoose from 'mongoose';
+import Notification from '../models/notification.model';
 
 export const createPost = async (req: Request, res: Response) => {
   try {
@@ -34,6 +36,128 @@ export const createPost = async (req: Request, res: Response) => {
     return res.status(201).json({ status: 'success', post: newPost });
   } catch (error) {
     console.log('Error in create post controller', error);
+    res
+      .status(500)
+      .json({ status: 'failed', message: 'Internal server error' });
+  }
+};
+
+export const likeUnlikePost = async (req: Request, res: Response) => {
+  try {
+    const { postId } = req.params;
+    const currentUserId = req.user?._id as mongoose.Types.ObjectId;
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({
+        status: 'failed',
+        message: 'Post not found',
+      });
+    }
+
+    const isLikedByUser = post.likes.includes(currentUserId);
+
+    if (isLikedByUser) {
+      await Post.updateOne(
+        { _id: postId },
+        { $pull: { likes: currentUserId } },
+      );
+      res.status(200).json({
+        status: 'success',
+        message: 'Post successfully unliked',
+        postId,
+      });
+    } else {
+      post.likes.push(currentUserId);
+      await post.save();
+
+      const notification = new Notification({
+        from: currentUserId,
+        to: post.user,
+        type: 'like',
+      });
+      await notification.save();
+
+      res.status(200).json({
+        status: 'success',
+        message: 'Post successfully liked',
+        postId,
+      });
+    }
+  } catch (error) {
+    console.log('Error in create post controller', error);
+    res
+      .status(500)
+      .json({ status: 'failed', message: 'Internal server error' });
+  }
+};
+
+export const deletePost = async (req: Request, res: Response) => {
+  try {
+    const { postId } = req.params;
+    const currentUserId = req.user?._id as mongoose.Types.ObjectId;
+
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({
+        status: 'failed',
+        message: 'Post not found',
+      });
+    }
+
+    const isUserAuthorized = post.user.toString() === currentUserId.toString();
+
+    if (!isUserAuthorized) {
+      return res.status(403).json({
+        status: 'failed',
+        message: 'You only can delete your posts',
+      });
+    }
+
+    const imgId = post.img?.split('/').pop()?.split('.')[0];
+    if (imgId) {
+      await cloudinary.uploader.destroy(imgId);
+    }
+
+    await Post.findByIdAndDelete(postId);
+    res
+      .status(200)
+      .json({ status: 'success', message: 'Post deleted successfully' });
+  } catch (error) {
+    console.log('Error in delete post controller', error);
+    res
+      .status(500)
+      .json({ status: 'failed', message: 'Internal server error' });
+  }
+};
+
+export const commentOnPost = async (req: Request, res: Response) => {
+  try {
+    const { postId } = req.params;
+    const { text } = req.body;
+    const currentUserId = req.user?._id as mongoose.Types.ObjectId;
+
+    if (!text) {
+      return res.status(400).json({
+        status: 'failed',
+        message: "Comment couldn't be empty",
+      });
+    }
+
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({
+        status: 'failed',
+        message: 'Post not found',
+      });
+    }
+
+    const comment = { text, user: currentUserId };
+    post.comments.push(comment);
+    await post.save();
+    res.status(200).json({ status: 'success', post, comment });
+  } catch (error) {
+    console.log('Error in comment on post controller', error);
     res
       .status(500)
       .json({ status: 'failed', message: 'Internal server error' });
--- api/src/routes/post.route.ts
+++ api/src/routes/post.route.ts
@@ -1,12 +1,17 @@
 import { Router } from 'express';
 import authMiddleware from '../middlewares/auth.middleware';
-import { createPost } from '../controllers/post.contoller';
+import {
+  commentOnPost,
+  createPost,
+  deletePost,
+  likeUnlikePost,
+} from '../controllers/post.contoller';
 
 const router = Router();
 
 router.route('/create').post(authMiddleware, createPost);
-// router.route('/like/:postId').post(authMiddleware, likeUnlikePost);
-// router.route('/comment/:postId').post(authMiddleware, commentOnPost);
-// router.route('/:postId').delete(authMiddleware, deletePost);
+router.route('/like/:postId').post(authMiddleware, likeUnlikePost);
+router.route('/comment/:postId').post(authMiddleware, commentOnPost);
+router.route('/:postId').delete(authMiddleware, deletePost);
 
 export default router;
