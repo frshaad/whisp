1fda2aaccae54e04de08bc9277efc03e26b9e47b
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2024-10-03 12:00:20 +0330

    chore(clinet): add react query eslint plugin rulesets

--- client/src/context/react-query-provider.tsx
+++ client/src/context/react-query-provider.tsx
@@ -2,13 +2,14 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useState } from 'react';
 
 type Props = {
   children: React.ReactNode;
 };
 
 export default function ReactQueryProvider({ children }: Props) {
-  const queryClient = new QueryClient();
+  const [queryClient] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
