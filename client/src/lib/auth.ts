b77a234af4c7a4b679a6e2d72ed1ffc51285eaa7
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2024-10-06 12:24:36 +0330

    refactor(client): get authenticated user data server function

--- client/src/lib/auth.ts
+++ client/src/lib/auth.ts
@@ -1,8 +1,23 @@
-import { User } from '@/types/user';
+import { AuthActionResult } from '@/types/auth-actions';
 
 import api from './api';
 
-export const getUser = async (): Promise<User> => {
-  const { data } = await api.get('/auth/me');
-  return data;
+export const getAuthUser = async (): Promise<AuthActionResult> => {
+  try {
+    const { data } = await api.get('/auth/me');
+    return { status: 'success', user: data.user };
+  } catch (error: any) {
+    if (error.response && error.response.data) {
+      return {
+        status: 'failed',
+        message:
+          error.response.data.message || 'Failed to get authenticated user.',
+      };
+    }
+
+    return {
+      status: 'failed',
+      message: error.message || 'An unknown error occurred.',
+    };
+  }
 };
