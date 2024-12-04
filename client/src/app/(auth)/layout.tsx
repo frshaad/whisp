6e71a62618ba951475393360fb27668772339ed3
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2024-12-05 00:41:41 +0330

    refactor(client): remove image from auth form pages

--- client/src/app/(auth)/layout.tsx
+++ client/src/app/(auth)/layout.tsx
@@ -1,8 +1,6 @@
 import { cookies } from 'next/headers';
-import Image from 'next/image';
 import { redirect } from 'next/navigation';
 
-import FriendsImg from '@/assets/friends.webp';
 import Logo from '@/components/shared/logo';
 
 export default function AuthLayout({
@@ -17,18 +15,10 @@ export default function AuthLayout({
   }
 
   return (
-    <div className="h-screen w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+    <div className="flex h-screen w-full items-center justify-center">
       <div className="flex flex-col items-center justify-center gap-16 py-12">
         <Logo />
         {children}
-      </div>
-      <div className="hidden h-screen bg-muted lg:block">
-        <Image
-          src={FriendsImg}
-          alt="friends"
-          priority
-          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
       </div>
     </div>
   );
--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -8,7 +8,7 @@ export default function LoginPage() {
       <div className="grid gap-2 text-center">
         <h1 className="text-3xl font-bold">Login</h1>
         <p className="text-balance text-muted-foreground">
-          Enter your email below to login to your account
+          Enter your credentials below to login to your account
         </p>
       </div>
       <LoginForm />
